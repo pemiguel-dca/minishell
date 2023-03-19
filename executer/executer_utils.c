1ec2a1f3a2fb393204f9acbe4d40e4d486ca8fc1
Pedro <pedronovo-16@hotmail.com>
2023-03-19 02:55:02 +0000

    Norm fixex, correct dispatch_string function in lexer

--- executer/executer_utils.c
+++ executer/executer_utils.c
@@ -6,7 +6,7 @@
 /*   By: pemiguel <pemiguel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 17:12:05 by pemiguel          #+#    #+#             */
-/*   Updated: 2023/03/19 02:44:30 by pemiguel         ###   ########.fr       */
+/*   Updated: 2023/03/19 02:54:13 by pemiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,33 @@ t_executer	*initialize_executer_params(t_vec *expressions)
 	if (files_to_be_created(expressions) && params->i == 0)
 		params->new_files = create_files(expressions);
 	return (params);
+}
+
+char	*bin_path(t_expression expr)
+{
+	char		*res;
+	char		*with_delim;
+	char		*full_path;
+	char		**path_env;
+	size_t		i;
+
+	res = NULL;
+	with_delim = ft_strjoin("/", expr.args.buf[0]);
+	path_env = ft_split(getenv("PATH"), ':');
+	i = 0;
+	while (path_env[i])
+	{
+		full_path = ft_strjoin(path_env[i], with_delim);
+		if (access(full_path, F_OK) == 0)
+		{
+			res = full_path;
+			break ;
+		}
+		free(full_path);
+		i++;
+	}
+	free(with_delim);
+	return (res);
 }
 
 void	execute_cmd(t_expression *expr)
